ca09fbfd6e0ea3ad75a6691a98f9b2d97fb35c0e
Thomas M Kehrenberg <tmke8@posteo.net>
2026-03-14 01:24:15 +0100

    Add draft of build script

---
+++ LibertinusMath/convert_to_otf.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+set -e
+sfdnormalize sources/LibertinusMath-Regular.sfd build/LibertinusMath-Regular-normalized.sfd
+
+#sfd2ufo build/LibertinusMath-Regular-normalized.sfd build/LibertinusMath-Regular-normalized.ufo
+#ufonormalizer build/LibertinusMath-Regular-normalized.ufo
+#fontmake --verbose WARNING --fea-include-dir sources/features -u build/LibertinusMath-Regular-normalized.ufo -o otf --output-dir build
+#mv build/LibertinusMath-Regular-normalized.otf build/LibertinusMath-Regular-instance.otf
+
+python tools/build.py --input=build/LibertinusMath-Regular-normalized.sfd --output=build/LibertinusMath-Regular-instance.otf --feature-file=sources/features/gsub.fea
+
+psautohint -o build/LibertinusMath-Regular-instance-hinted.otf build/LibertinusMath-Regular-instance.otf
+python -m cffsubr -o build/LibertinusMath-Regular-subr.otf build/LibertinusMath-Regular-instance-hinted.otf
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python gftools fix-font build/LibertinusMath-Regular-subr.otf -o build/LibertinusMath-Regular.otf
+font-v write --ver=7-051 --dev --sha1 build/LibertinusMath-Regular.otf
